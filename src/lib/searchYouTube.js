c6b6b0ec378cc6873e48e73afebe46a458d586c3
shams-ali <shams.ali0216@gmail.com>
2016-05-07 11:05:12 -0700

    youtube grab works

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,20 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  var settings = {
+    key: options.key,
+    q: options.query,
+    maxResults: options.max,
+    part: 'snippet',
+    type: 'video',
+    videoEmbeddable: true
+    
+  };
+  $.ajax({
+    url: 'https://www.googleapis.com/youtube/v3/search/',
+    type: 'GET',
+    data: settings,
+    success: (data) => callback(data.items),
+    error: (e) => console.log(e, 'did not work')
+  });
 };
 
 window.searchYouTube = searchYouTube;
